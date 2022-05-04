b9f598e21b87343d9532fc24d863e1d96d7812d0
oracle2_dev <assia-jean.ngoran@wealthtechinnovations.com>
2022-05-04 14:31:48 +0000

    modification de la page de transfert

--- pages/TransfertNational/PageTransfert.js
+++ pages/TransfertNational/PageTransfert.js
@@ -3,7 +3,9 @@ import React from 'react'
 import { Header, Icon } from 'react-native-elements'
 import { Button, TextInput } from 'react-native-paper'
 
-const PageTransfert = ({navigation}) => {
+const PageTransfert = ({route,navigation}) => {
+    const nom = route.params.userName
+    const phone = route.params.userPhone
   return (
     <View style={styles.container}>
         <Header
@@ -17,17 +19,14 @@ const PageTransfert = ({navigation}) => {
             }
       />
         <View style={{flex:1, padding:10}}>
-            <Text style={{fontWeight:"bold", fontSize:20, marginBottom:10}}>Montant</Text>
-            <Text>À</Text>
-            <Text>Assia Jean</Text>
-            <TextInput disabled value='+2250709483463' style={{height:45, marginBottom:10}} />
-            <Text style={{fontSize:16, marginBottom:8}}>Saisissez le montant à transférer</Text>
-            <TextInput label='Montant envoyé' keyboardType="number-pad" autoFocus placeholder='Entrer le montant' style={{marginBottom:10, height:60}} />
-            <TextInput label='Montant reçu' keyboardType="number-pad" autoFocus placeholder='Entrer le montant' style={{marginBottom:10, height:60}} />
+            <Text style={{textAlign:"center", fontSize:17,}}>À {nom}</Text>
+            <Text style={{textAlign:"center", fontSize:17, marginBottom:20}}>{phone}</Text>
+            <TextInput label='Montant à envoyer' keyboardType="number-pad" autoFocus placeholder='Entrer le montant' style={{marginBottom:10, height:60, backgroundColor: "#fff",}} />
+            <TextInput label='Montant à recevoir' keyboardType="number-pad" autoFocus placeholder='Entrer le montant' style={{marginBottom:10, height:60, backgroundColor: "#fff",}} />
 
             <Text style={{textAlign:"center", marginTop:10}}>Frais paychap = 1%</Text>
             <Text style={{textAlign:"center", marginTop:10}}>Frais maximum 5.000F</Text>
-            <Button icon="login" style={styles.boutonLogin} mode="contained"  onPress={() => navigation.navigate('Forms')}>
+            <Button style={styles.boutonLogin} mode="contained"  onPress={() => navigation.navigate('Tabs')}>
                     Transférer
                 </Button>
 
@@ -50,6 +49,7 @@ const styles = StyleSheet.create({
         marginBottom:30,
         backgroundColor:"#1E89E2",
         position:"absolute",
-        bottom:0
+        bottom:0,
+        borderRadius:18
     },
 })
